eb988dfe2527b60c49c15a29059e86a4e52fb986
William <wguiraud@protonmail.com>
2024-03-11 16:19:02 -0700

    addmmit -m

--- todo.rb
+++ todo.rb
@@ -16,15 +16,18 @@ get "/" do
   redirect "/lists"
 end
 
+# view all lists
 get "/lists" do 
   @lists = session[:lists]
   erb :lists 
 end
 
+# render the new list form 
 get "/lists/new" do 
   erb :new_list
 end
 
+# create a new list
 post "/lists" do 
   session[:lists] << { name: params[:list_name], todos: [] }
   redirect "/lists"
