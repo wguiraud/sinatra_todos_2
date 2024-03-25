cf59bc4026c2d450ec38bb2cec3f249f72222948
William <wguiraud@protonmail.com>
2024-03-25 14:33:29 -0700

    implementing sending an ajax request when a todo is deleted.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,7 +6,20 @@ $(function() {
 
     var ok = confirm("Are you sure? this cannot be undone!"); 
     if (ok) { 
-      this.submit();
+      //this.submit();
+      var form = $(this); 
+
+      var request = $.ajax({
+        url: form.attr("action"), 
+        method: form.attr("method")
+      });
+
+      request.done(function(data, textStatus, jqXHR) { 
+        form.parent('li').remove()
+      });
+
+      //request.fail(function() {} })' in production env
+
     }
 
   });
